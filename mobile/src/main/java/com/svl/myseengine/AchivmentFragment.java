8b5bc31bb5bb66696a599c9c3b5ec69bdeb27cbc
JustDev <savelijln0@gmail.com>
2024-04-24 22:50:23 +0400

    Achievemnt is now rounded (and transparent)!

--- mobile/src/main/java/com/svl/myseengine/AchivmentFragment.java
+++ mobile/src/main/java/com/svl/myseengine/AchivmentFragment.java
@@ -1,6 +1,8 @@
 package com.svl.myseengine;
 
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,13 +14,16 @@ public class AchivmentFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Задайте макет для фрагмента
-
         View view = inflater.inflate(R.layout.achivment_unlocked, container, false);
-        // Задайте размеры для фрагмента
 
+        // Установите прозрачный фон для диалогового окна
+        getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        // Задайте размеры для фрагмента
         int width = getResources().getDisplayMetrics().widthPixels;
         int height = getResources().getDisplayMetrics().heightPixels / 6;
         getDialog().getWindow().setLayout(width, height);
+
         return view;
     }
 }
